a338252ab69020050b79b4b09fab2b6b61f70442
Renal Apriansyah <renal.apr@gmail.com>
2021-10-09 16:03:42 +0700

    dynamic be endpoint

--- src/outbound/comment.ts
+++ src/outbound/comment.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import { CommentRequestDto } from '../dto/CommentRequestDto';
 
+const url = process.env.BE_ENDPOINT;
 interface Page {
   page: string, 
   limit: string
 }
 
 export const createComment = (requestDto: CommentRequestDto) => {
-  return axios.post('http://localhost:3001/comment', requestDto)
+  return axios.post(`${url}/comment`, requestDto)
 }
 
 export const getComment = ({ page, limit}: Page) => {
-  return axios.get(`http://localhost:3001/comment?page=${page}&limit=${limit}`)
+  return axios.get(`${url}/comment?page=${page}&limit=${limit}`)
   .then(res => res.data)
 }
